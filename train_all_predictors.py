4f127dfb3d84c95946a264784f8c6efa160c825f
Arnav Malawade <malawada@uci.edu>
2020-03-29 11:38:30 -0700

    training for all predictors updated

--- train_all_predictors.py
+++ train_all_predictors.py
@@ -1,7 +1,7 @@
 from collections import defaultdict
 import argparse
 import train_predictor
-import multiprocessing
+import os
 
 parser = argparse.ArgumentParser(description='PyTorch RNN Prediction Model on Time-series Dataset')
 parser.add_argument('--data', type=str, default='IMS_bearings_test2',
@@ -62,60 +62,62 @@ args = parser.parse_args()
 
 
 files = defaultdict()
-files['IMS_bearings_test1']=("combinedfiles_s4.pkl",
-                        "combinedfiles_s5.pkl",
-                        "combinedfiles_s6.pkl",
-                        "combinedfiles_s7.pkl")
-files['IMS_bearings_test2']=("combinedfiles_s0.pkl")
-files['IMS_bearings_test3']=("combinedfiles_s2.pkl")
-files['FEMTO_bearings_1_1']=("combinedfiles_accel_s0.pkl",
-				"combinedfiles_accel_s1.pkl")
-files['FEMTO_bearings_1_2']=("combinedfiles_accel_s0.pkl",
-				"combinedfiles_accel_s1.pkl")
-files['FEMTO_bearings_2_1']=("combinedfiles_accel_s0.pkl",
-				"combinedfiles_accel_s1.pkl")
-files['FEMTO_bearings_2_2']=("combinedfiles_accel_s0.pkl",
-				"combinedfiles_accel_s1.pkl")
-files['FEMTO_bearings_3_1']=("combinedfiles_accel_s0.pkl",
-				"combinedfiles_accel_s1.pkl")
-files['FEMTO_bearings_3_2']=("combinedfiles_accel_s0.pkl",
-				"combinedfiles_accel_s1.pkl")
-files['Bearing_1_3']=("Bearing_1_3combinedfiles_accel_s0.pkl",
-				"Bearing_1_3combinedfiles_accel_s1.pkl")
-files['Bearing_1_4']=("Bearing_1_4combinedfiles_accel_s0.pkl",
-				"Bearing_1_4combinedfiles_accel_s1.pkl")
-files['Bearing_1_5']=("Bearing_1_5combinedfiles_accel_s0.pkl",
-				"Bearing_1_5combinedfiles_accel_s1.pkl")
-files['Bearing_1_6']=("Bearing_1_6combinedfiles_accel_s0.pkl",
-				"Bearing_1_6combinedfiles_accel_s1.pkl")
-files['Bearing_1_7']=("Bearing_1_7combinedfiles_accel_s0.pkl",
-				"Bearing_1_7combinedfiles_accel_s1.pkl")
-files['Bearing_2_3']=("Bearing_2_3combinedfiles_accel_s0.pkl",
-				"Bearing_2_3combinedfiles_accel_s1.pkl")
-files['Bearing_2_4']=("Bearing_2_4combinedfiles_accel_s0.pkl",
-				"Bearing_2_4combinedfiles_accel_s1.pkl")
-files['Bearing_2_5']=("Bearing_2_5combinedfiles_accel_s0.pkl",
-				"Bearing_2_5combinedfiles_accel_s1.pkl")
-files['Bearing_2_6']=("Bearing_2_6combinedfiles_accel_s0.pkl",
-				"Bearing_2_6combinedfiles_accel_s1.pkl")
-files['Bearing_2_7']=("Bearing_2_7combinedfiles_accel_s0.pkl",
-				"Bearing_2_7combinedfiles_accel_s1.pkl")
-files['Bearing_3_3']=("Bearing_3_3combinedfiles_accel_s0.pkl",
-				"Bearing_3_3combinedfiles_accel_s1.pkl")
+# files['IMS_bearings_test1']=("combinedfiles_s4.pkl",
+                        # "combinedfiles_s5.pkl",
+                        # "combinedfiles_s6.pkl",
+                        # "combinedfiles_s7.pkl")
+# files['IMS_bearings_test2']=(["combinedfiles_s0.pkl"])
+# files['IMS_bearings_test3']=(["combinedfiles_s2.pkl"])
+# files['FEMTO_bearings_1_1']=("combinedfiles_accel_s0.pkl",
+				# "combinedfiles_accel_s1.pkl")
+# files['FEMTO_bearings_1_2']=("combinedfiles_accel_s0.pkl",
+				# "combinedfiles_accel_s1.pkl")
+# files['FEMTO_bearings_2_1']=("combinedfiles_accel_s0.pkl",
+				# "combinedfiles_accel_s1.pkl")
+# files['FEMTO_bearings_2_2']=("combinedfiles_accel_s0.pkl",
+				# "combinedfiles_accel_s1.pkl")
+# files['FEMTO_bearings_3_1']=("combinedfiles_accel_s0.pkl",
+				# "combinedfiles_accel_s1.pkl")
+# files['FEMTO_bearings_3_2']=("combinedfiles_accel_s0.pkl",
+				# "combinedfiles_accel_s1.pkl")
+files['Bearing1_3']=("Bearing1_3combinedfiles_accel_s0.pkl",
+				"Bearing1_3combinedfiles_accel_s1.pkl")
+files['Bearing1_4']=("Bearing1_4combinedfiles_accel_s0.pkl",
+				"Bearing1_4combinedfiles_accel_s1.pkl")
+files['Bearing1_5']=("Bearing1_5combinedfiles_accel_s0.pkl",
+				"Bearing1_5combinedfiles_accel_s1.pkl")
+files['Bearing1_6']=("Bearing1_6combinedfiles_accel_s0.pkl",
+				"Bearing1_6combinedfiles_accel_s1.pkl")
+files['Bearing1_7']=("Bearing1_7combinedfiles_accel_s0.pkl",
+				"Bearing1_7combinedfiles_accel_s1.pkl")
+files['Bearing2_3']=("Bearing2_3combinedfiles_accel_s0.pkl",
+				"Bearing2_3combinedfiles_accel_s1.pkl")
+files['Bearing2_4']=("Bearing2_4combinedfiles_accel_s0.pkl",
+				"Bearing2_4combinedfiles_accel_s1.pkl")
+files['Bearing2_5']=("Bearing2_5combinedfiles_accel_s0.pkl",
+				"Bearing2_5combinedfiles_accel_s1.pkl")
+files['Bearing2_6']=("Bearing2_6combinedfiles_accel_s0.pkl",
+				"Bearing2_6combinedfiles_accel_s1.pkl")
+files['Bearing2_7']=("Bearing2_7combinedfiles_accel_s0.pkl",
+				"Bearing2_7combinedfiles_accel_s1.pkl")
+files['Bearing3_3']=("Bearing3_3combinedfiles_accel_s0.pkl",
+				"Bearing3_3combinedfiles_accel_s1.pkl")
                
 
 for dir, filelist in files.items():
     for f in filelist:
         args.data = dir
         args.filename = f
-        
-        try:
-            train_predictor.main(args)
-        except RuntimeError as err:
-            print("EXCEPTION OCCURRED")
-            print(err)
-            with open("errlog.txt",'a+') as f:
-                f.write(err)
-                f.write("\n\n")
+        if(os.path.exists('dataset/'+dir+'/labeled/train/'+f)):
+            try:
+                train_predictor.main(args)
+            except RuntimeError as err:
+                print("EXCEPTION OCCURRED")
+                print(err)
+                with open("errlog.txt",'a+') as f:
+                    f.write(err)
+                    f.write("\n\n")
+        else:
+            print("File does not exist! filename:" + dir + '/' + f)
         
 print("training of all files completed")

de88eca587280e7e0454cfb85bd52593b56c5c35
Arnav Malawade <malawada@users.noreply.github.com>
2020-12-02 22:07:50 -0800

    replace old SRU with sru lib

--- model/model.py
+++ model/model.py
@@ -18,9 +18,8 @@ class RNNPredictor(nn.Module):
         if rnn_type in ['LSTM', 'GRU']:
             self.rnn = getattr(nn, rnn_type)(rnn_inp_size, rnn_hid_size, nlayers, dropout=dropout)
         elif rnn_type == 'SRU':
-            from cuda_functional import SRU, SRUCell
-            self.rnn = SRU(input_size=rnn_inp_size,hidden_size=rnn_hid_size,num_layers=nlayers,dropout=dropout,
-                           use_tanh=False,use_selu=True,layer_norm=True)
+            from sru import SRU, SRUCell
+            self.rnn = SRU(input_size=rnn_inp_size,hidden_size=rnn_hid_size,num_layers=nlayers,dropout=dropout, layer_norm=True)
         else:
             try:
                 nonlinearity = {'RNN_TANH': 'tanh', 'RNN_RELU': 'relu'}[rnn_type]

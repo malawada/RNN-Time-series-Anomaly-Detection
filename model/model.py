0d8f8e68e79c02fc1dd70c147cf6a1a66394d751
Arnav Malawade <34730799+malawada@users.noreply.github.com>
2020-12-10 02:00:46 -0800

    add save path

--- model/model.py
+++ model/model.py
@@ -95,13 +95,13 @@ class RNNPredictor(nn.Module):
     def save_checkpoint(self,state, is_best):
         print("=> saving checkpoint ..")
         args = state['args']
-        checkpoint_dir = Path('save',args.model,args.data,'checkpoint')
+        checkpoint_dir = Path(args.save_path,args.model,args.data,'checkpoint')
         checkpoint_dir.mkdir(parents=True,exist_ok=True)
         checkpoint = checkpoint_dir.joinpath(args.filename).with_suffix('.pth')
 
         torch.save(state, checkpoint)
         if is_best:
-            model_best_dir = Path('save',args.model,args.data,'model_best')
+            model_best_dir = Path(args.save_path,args.model,args.data,'model_best')
             model_best_dir.mkdir(parents=True,exist_ok=True)
             shutil.copyfile(checkpoint, model_best_dir.joinpath(args.filename).with_suffix('.pth'))
 
